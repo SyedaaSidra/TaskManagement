b09ee7b357a93b7f221852140d6eb608e492024e
sidraRashdi <rashdimuntaha557@gmail.com>
2025-07-16 06:32:10 +0500

    Set up React Router with BrowserRouter and created base routes for Login, Register, and Dashboard

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/dashboard" element={<Dashboard />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
