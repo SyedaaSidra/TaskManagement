eb955fcab039473d87e36d84723ee5eebac273dd
sidraRashdi <rashdimuntaha557@gmail.com>
2025-07-20 00:08:53 +0500

    Protect dashboard route using AuthGuard and token check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import AuthGuard from "./components/AuthGuard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
       <Route path="/register" element={<Register />}></Route>
-      <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route
+        path="/dashboard"
+        element={
+          <AuthGuard>
+            <Dashboard />
+          </AuthGuard>
+        }
+      />
     </Routes>
   );
 }
